7b95339b0a45354903208117464ac10976f15538
none <none>
2023-03-29 14:47:35 +0800

    优化初次加载loading

--- src/pages/loading/index.tsx
+++ src/pages/loading/index.tsx
@@ -1,10 +1,11 @@
 import { Spin } from "antd";
+import styles from "./index.module.scss";
 
 const LoadingPage = () => {
   return (
-    <>
+    <div className={styles.loadingBox}>
       <Spin />
-    </>
+    </div>
   );
 };
 
