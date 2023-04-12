2a5eab39a648fa860070fa7a9bd68c69c6343f12
none <none>
2023-04-12 11:39:03 +0800

    优化代码

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,6 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import store from "./store";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter } from "react-router-dom";
 import { ConfigProvider } from "antd";
 import zhCN from "antd/locale/zh_CN";
@@ -21,8 +19,3 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
     </ConfigProvider>
   </Provider>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
