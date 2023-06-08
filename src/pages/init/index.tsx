c2420798d21aec7ec8d50a786c55c03998afa473
禺狨 <18119604035@163.com>
2023-06-08 18:16:18 +0800

    页面初始化优化

--- src/pages/init/index.tsx
+++ src/pages/init/index.tsx
@@ -22,65 +22,77 @@ export const InitPage = (props: Props) => {
   const [showHeader, setShowHeader] = useState<boolean>(true);
   const [showNoHeader, setShowNoHeader] = useState<boolean>(false);
   const [showFooter, setShowFooter] = useState<boolean>(true);
-  if (props.loginData) {
-    dispatch(loginAction(props.loginData));
-  }
-  if (props.configData) {
-    let config: SystemConfigStoreInterface = {
-      //系统配置
-      systemApiUrl: props.configData["system-api-url"],
-      systemH5Url: props.configData["system-h5-url"],
-      systemLogo: props.configData["system-logo"],
-      systemName: props.configData["system-name"],
-      systemPcUrl: props.configData["system-pc-url"],
-      pcIndexFooterMsg: props.configData["system-pc-index-footer-msg"],
-      //播放器配置
-      playerPoster: props.configData["player-poster"],
-      playerIsEnabledBulletSecret:
-        props.configData["player-is-enabled-bullet-secret"] &&
-        props.configData["player-is-enabled-bullet-secret"] === "1"
-          ? true
-          : false,
-      playerIsDisabledDrag:
-        props.configData["player-disabled-drag"] &&
-        props.configData["player-disabled-drag"] === "1"
-          ? true
-          : false,
-      playerBulletSecretText: props.configData["player-bullet-secret-text"],
-      playerBulletSecretColor: props.configData["player-bullet-secret-color"],
-      playerBulletSecretOpacity:
-        props.configData["player-bullet-secret-opacity"],
-    };
-    dispatch(saveConfigAction(config));
-  }
-
+  const [init, setInit] = useState<boolean>(false);
   useEffect(() => {
-    setShowHeader(true);
-    setShowNoHeader(false);
-    setShowFooter(true);
-    if (pathname === "/login") {
-      setShowNoHeader(true);
-      setShowHeader(false);
-      setShowFooter(false);
-    } else if (!params.hourId) {
+    if (pathname) {
+      if (pathname === "/login") {
+        setShowNoHeader(true);
+        setShowHeader(false);
+        setShowFooter(false);
+      } else if (!params.hourId) {
+        setShowNoHeader(false);
+        setShowHeader(true);
+        setShowFooter(true);
+      } else {
+        setShowNoHeader(false);
+        setShowHeader(false);
+        setShowFooter(false);
+      }
+    }
+
+    return () => {
+      // 组件卸载时，清除定时器
       setShowNoHeader(false);
       setShowHeader(true);
       setShowFooter(true);
-    } else {
-      setShowNoHeader(false);
-      setShowHeader(false);
-      setShowFooter(false);
-    }
+    };
   }, [pathname, params]);
+
+  useEffect(() => {
+    if (props.loginData) {
+      dispatch(loginAction(props.loginData));
+    }
+    if (props.configData) {
+      let config: SystemConfigStoreInterface = {
+        //系统配置
+        systemApiUrl: props.configData["system-api-url"],
+        systemH5Url: props.configData["system-h5-url"],
+        systemLogo: props.configData["system-logo"],
+        systemName: props.configData["system-name"],
+        systemPcUrl: props.configData["system-pc-url"],
+        pcIndexFooterMsg: props.configData["system-pc-index-footer-msg"],
+        //播放器配置
+        playerPoster: props.configData["player-poster"],
+        playerIsEnabledBulletSecret:
+          props.configData["player-is-enabled-bullet-secret"] &&
+          props.configData["player-is-enabled-bullet-secret"] === "1"
+            ? true
+            : false,
+        playerIsDisabledDrag:
+          props.configData["player-disabled-drag"] &&
+          props.configData["player-disabled-drag"] === "1"
+            ? true
+            : false,
+        playerBulletSecretText: props.configData["player-bullet-secret-text"],
+        playerBulletSecretColor: props.configData["player-bullet-secret-color"],
+        playerBulletSecretOpacity:
+          props.configData["player-bullet-secret-opacity"],
+      };
+      dispatch(saveConfigAction(config));
+    }
+    setInit(true);
+  }, [props]);
 
   return (
     <>
-      <div>
-        {showNoHeader && <NoHeader></NoHeader>}
-        {showHeader && <Header></Header>}
-        <Outlet />
-        {showFooter && <Footer></Footer>}
-      </div>
+      {init && (
+        <div>
+          {showNoHeader && <NoHeader></NoHeader>}
+          {showHeader && <Header></Header>}
+          <Outlet />
+          {showFooter && <Footer></Footer>}
+        </div>
+      )}
     </>
   );
 };
