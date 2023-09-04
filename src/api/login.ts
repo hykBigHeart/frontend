3af8b4eb27eb0cba6f81c8f15fe40846c7ca7792
unknown <18119604035@163.com>
2023-09-04 16:50:44 +0800

    学员登录ldap优化

--- src/api/login.ts
+++ src/api/login.ts
@@ -10,3 +10,11 @@ export function login(email: string, password: string) {
 export function logout() {
   return client.post("/api/v1/auth/logout", {});
 }
+
+export function loginLdap(email: string, password: string) {
+  return client.post("/api/v1/auth/login/ldap", {
+    username: email,
+    password: password,
+  });
+}
+
