96da44a884706d7fb55abb0fe2f3c89cfd9767f9
xxx <x@x.x>
2024-02-20 14:59:07 +0800

    移除 react-ga 依赖

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,13 @@
-import { Suspense, useEffect } from "react";
-import ReactGA from "react-ga";
-import { useLocation, useRoutes } from "react-router-dom";
+import { Suspense } from "react";
+import { useRoutes } from "react-router-dom";
 import routes from "./routes";
 import "./App.scss";
 import LoadingPage from "./pages/loading";
 
-const G_ID = import.meta.env.VITE_G_ID || "";
-if (G_ID) {
-  ReactGA.initialize(G_ID);
-}
-
 const App = () => {
   const views = useRoutes(routes);
 
-  const location = useLocation();
-  useEffect(() => {
-    if (!G_ID) {
-      return;
-    }
-    ReactGA.pageview(location.pathname + location.search);
-  }, [location]);
-
-  return (
-    <Suspense fallback={<LoadingPage />}>
-      {views}
-    </Suspense>
-  );
+  return <Suspense fallback={<LoadingPage />}>{views}</Suspense>;
 };
 
 export default App;
